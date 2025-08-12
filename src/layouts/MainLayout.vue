44e9871c7d145ae6c43f94199febd3d06810c323
Allan Chica <ninjalover249@gmail.com>
2025-08-11 22:08:33 -0300

    fix: content height

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center h-full">
-    <div class="flex-1 w-full min-h-0">
+    <div class="flex-1 w-full min-h-0 overflow-y-auto">
       <router-view></router-view>
     </div>
 
